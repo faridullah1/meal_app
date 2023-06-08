07520630e7b8c39132fd9aa9286dfd167b4944bb
faridullah1 <faridullah996@gmail.com>
2023-06-08 11:35:34 +0500

    selected category meal items

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/meal_item.dart';
+import '../dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+
+  const CategoryMealsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'] as String;
+    final categoryId = routeArgs['id'] as String;
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+        backgroundColor: Colors.pink,
+      ),
+      body: Center(
+        child: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return MealItem(
+              title: categoryMeals[index].title,
+              imageUrl: categoryMeals[index].imageUrl,
+              duration: categoryMeals[index].duration,
+              complexity: categoryMeals[index].complexity,
+              affordability: categoryMeals[index].affordability,
+            );
+          },
+          itemCount: categoryMeals.length,
+        ),
+      ),
+    );
+  }
+}
